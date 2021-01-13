9c40d56612c858e351335f316c7251e5a10d1eea
James Murphy <jamesthebald@gmail.com>
2021-01-13 16:30:32 -0700

    commented out a console.log we put in for debugging

--- zback/app/controllers/tutorial.controller.js
+++ zback/app/controllers/tutorial.controller.js
@@ -32,7 +32,7 @@ exports.create = (req, res) => {
 
 // Retrieve all Tutorials from the database.
 exports.findAll = (req, res) => {
-  console.log("Running findAll...");
+  // console.log("Running findAll...");
   const title = req.query.title;
   var condition = title ? { title: { $regex: new RegExp(title), $options: "i" } } : {};
 
