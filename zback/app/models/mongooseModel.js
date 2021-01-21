46f01448d2b5f1de88f4fc106084dca030d12aa2
James Murphy <jamesthebald@gmail.com>
2021-01-20 20:56:39 -0700

    Changed comments only

--- zback/app/models/mongooseModel.js
+++ zback/app/models/mongooseModel.js
@@ -10,7 +10,7 @@ module.exports = (mongoose) => {
     { timestamps: true }
   );
 
-  const Posting = mongoose.model("posting", postingSchema, "postings");    // "posting" is the name of the collection in BeZKoder's database,
-                                                                      // while "postings" is the name of the collection in our db. 
+  const Posting = mongoose.model("postings", postingSchema);    // "postings" is the name of the collection in our database
+
   return Posting;
 };
