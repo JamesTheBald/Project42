7ba142ced4330b66e2b7d2ba25eabca48208214c
Collin <collinanda@gmail.com>
2021-02-01 13:42:19 -0700

    Added comment to test pushing branch to repo

--- zfront/src/App.js
+++ zfront/src/App.js
@@ -5,6 +5,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./styles/app.css";
 import PostingsList from "./components/PostingsList";
 
+// This comment was placed by Collin to test pushing this new branch to the remote repo on Github
+
 
 function App() {
   return (
