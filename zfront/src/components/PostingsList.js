6637b4f7bf4196b344c6109641a3cbbb5a1f0ac9
James Murphy <jamesthebald@gmail.com>
2021-02-08 22:39:29 -0700

    Hides the unselected search options

--- zfront/src/components/PostingsList.js
+++ zfront/src/components/PostingsList.js
@@ -30,12 +30,12 @@ const PostingsList = () => {
   const [postingsDataArray, setPostingsDataArray] = useState();
   const [currPostIndex, setCurrPostIndex] = useState(0); 
   const [postDraft, setPostDraft] = useState(emptyPost);
+  const [creatingPostFlag, setCreatingPostFlag] = useState(false);
   const [searchTitle, setSearchTitle] = useState("");
   const [searchTags, setSearchTags] = useState("");
   const [searchName, setSearchName] = useState("");
-  const [creatingPostFlag, setCreatingPostFlag] = useState(false);
+  const [searchTerm, setSearchTerm]=useState('Search by Title ');
   const [voteCount, setVoteCount] = useState(0);
-  const [searchTerm,setSearchTerm]=useState('Search by Title ');
 
 
   console.log("PostingsList.js begins: creatingPostFlag=", creatingPostFlag);
@@ -68,7 +68,7 @@ const PostingsList = () => {
 
           {/* 'Create Post' link */}
           <div
-            className="mx-4 hover:text-blue-400"
+            className="ml-4 mr-8 hover:text-blue-400"
             onClick={() => {
               console.log("PostingsList.js 'Create Post' Clicked. So creatingPostFlag=true");
               setCreatingPostFlag(true);
@@ -85,10 +85,16 @@ const PostingsList = () => {
 
 
           <DropdownButton
-            id='dropdown-variants-primary'
+
+// Pass in setPostingsDataArray
+// import onClickFindByTitle, onClickFindByTags, onClickFindByName
+// Put in component's JS code: handleSelect 
+// Declare in component's JS code: searchTerm, searchTitle, searchTags, searchName,
+ 
+            id='dropdown-variants-outline-primary'    // Make this dropdown smaller. See Sizing: 
+                                                      // https://react-bootstrap.netlify.app/components/dropdowns/#sizing 
             title={searchTerm}
             onSelect={handleSelect}
-            
           >
           <Dropdown.Item eventKey="Search by Title ">Search by Title</Dropdown.Item>
             <Dropdown.Item eventKey="Search by Tag ">Search by Tag</Dropdown.Item>
@@ -97,57 +103,75 @@ const PostingsList = () => {
 
 
           {/* Search by Title */}
-          <div className="flex flex-row mx-4">
-            <input
-              type="text"
-              className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
-              placeholder="Enter Title"
-              value={searchTitle}
-              onChange={ (event) => setSearchTitle(event.target.value)}
-            >
-            </input>
-            <button
-              className="ml-2 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
-              onClick={ () => onClickFindByTitle(searchTitle, setPostingsDataArray)}>
-              Search by Title
-            </button>
-          </div>
+          { (searchTerm === "Search by Title ") && (      // string must exactly match eventKey above
+            <div className="mx-4  flex flex-row">
+              <input
+                type="text"
+                className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
+                placeholder="Enter Title"
+                value={searchTitle}
+                onChange={ (event) => setSearchTitle(event.target.value)}
+              >
+              </input>
+              <button
+                className="ml-2 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
+                onClick={ () => onClickFindByTitle(searchTitle, setPostingsDataArray)}>
+                Search
+              </button>
+            </div>
+          )}
 
 
           {/* Search by Tag */}
-          <div className="flex flex-row mx-4">
-            <input
-              type="text"
-              className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
-              placeholder="Enter Tag"
-              value={searchTags}
-              onChange={(event) => setSearchTags(event.target.value)}
-            >
-            </input>
-            <button
-              className="ml-2 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
-              onClick={ () => onClickFindByTags(searchTags, setPostingsDataArray)}>
-              Search by Tag
-            </button>
-          </div>
-
+          { (searchTerm === "Search by Tag ") && (
+            <div className="flex flex-row mx-4">
+              <input
+                type="text"
+                className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
+                placeholder="Enter Tag"
+                value={searchTags}
+                onChange={(event) => setSearchTags(event.target.value)}
+              >
+              </input>
+              <button
+                className="ml-2 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
+                onClick={ () => onClickFindByTags(searchTags, setPostingsDataArray)}>
+                Search
+              </button>
+            </div>
+          )}
 
           {/* Search by Name */}
-          <div className="flex flex-row mx-4">
-            <input
-              type="text"
-              className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
-              placeholder="Enter Name"
-              value={searchName}
-              onChange={(event) => setSearchName(event.target.value)}
-            >
-            </input>
-            <button
-              className="ml-2 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
-              onClick={ () => onClickFindByName(searchName, setPostingsDataArray)}>
-              Search by Name
-            </button>
-          </div>
+          { (searchTerm === "Search by Name ") && (
+            <div className="flex flex-row ml-3">
+              <input
+                type="text"
+                className="w-100 p-1 text-gray-800 bg-gray-100 rounded-lg"
+                placeholder="Enter Name"
+                value={searchName}
+                onChange={(event) => setSearchName(event.target.value)}
+              >
+              </input>
+              <button
+                className="ml-3 px-3 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
+                onClick={ () => onClickFindByName(searchName, setPostingsDataArray)}>
+                Search
+              </button>
+            </div>
+          )}
+
+          <button
+            className="ml-3 px-2 py-1 text-gray-800 bg-gray-300 rounded-lg  hover:text-blue-600"
+            onClick={ () => {
+              setSearchTitle("")
+              onClickFindByTitle(searchTitle, setPostingsDataArray)
+              setSearchTags("")
+              onClickFindByTags(searchTags, setPostingsDataArray)
+              setSearchName("")
+              onClickFindByName(searchName, setPostingsDataArray)
+            }}>
+            Clear Search
+          </button>
 
         </div>
       </nav>
