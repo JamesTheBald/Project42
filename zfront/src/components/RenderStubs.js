1159f82d092994fc0a91e3185af09a9035067f23
Opeyemi Obayemi <opeyemi.obayemi@gmail.com>
2021-02-09 13:15:14 -0700

    updated RenderStubs with Draggable

--- zfront/src/components/RenderStubs.js
+++ zfront/src/components/RenderStubs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Tooltip from "./Tooltip";
 import PopupContent from "./PopupContent";
+import Draggable from "react-draggable"
 
 const RenderStubs = (props) => {
 
@@ -24,28 +25,28 @@ const RenderStubs = (props) => {
             <div key={indx} className="w-64 my-2">
               <Tooltip content={PopupContent(pst)} delay="200" direction="top" css="tooltipPopup rounded-lg"> 
                    {/* css="tooltipPopup" is required. Edit background color on tooltip.css */}
+                <Draggable>
+                  <div
+                    className="border p-2 border-gray-800 rounded-lg"
+                    onDoubleClick={() => {
+                      setCreatingPostFlag(false);
+                      setCurrPostIndex(indx);
+                      console.log("RenderStubs.js CurrPostIndex=",indx);
+                      setPostDraft(pst)
+                      setShowMainModal(true);
+                    }}
+                  >
 
-                <div
-                  className="border p-2 border-gray-800 rounded-lg"
-                  onClick={() => {
-                    setCreatingPostFlag(false);
-                    setCurrPostIndex(indx);
-                    console.log("RenderStubs.js CurrPostIndex=",indx);
-                    setPostDraft(pst)
-                    setShowMainModal(true);
-                  }}
-                >
-
-                  <div>
-                    { pst.title ? 
-                      <div>{pst.title}</div>
-                      :
-                      <div> Click to edit </div>
-                    }
-                    <div className="mt-2">{pst.contributors}</div>
+                    <div>
+                      { pst.title ? 
+                        <div>{pst.title}</div>
+                        :
+                        <div> Click to edit </div>
+                      }
+                      <div className="mt-2">{pst.contributors}</div>
+                    </div>
                   </div>
-
-                </div>
+                </Draggable>
               </Tooltip>
             </div>
           );
