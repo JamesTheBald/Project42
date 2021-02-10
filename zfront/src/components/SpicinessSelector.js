c5238106b5f20666b409731fb15c98829cbf3fba
Collin <collinanda@gmail.com>
2021-02-10 14:36:16 -0700

    Hid unselected peppers & added 'x' to reset selected spiciness in postDraft

--- zfront/src/components/SpicinessSelector.js
+++ zfront/src/components/SpicinessSelector.js
@@ -1,106 +1,161 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiChiliPepper } from 'react-icons/gi';
+import { TiDeleteOutline } from 'react-icons/ti';
 
 
 const SpicinessSelector = (props) => {
 
-
-
-  // Declare variables
   let postDraft = props.postDraft;
   const setPostDraft = props.setPostDraft;
 
 
 
-  // Declare functions
+  const [hideMildPepper, setHideMildPepper] = useState(false);
+  const [hideMediumPeppers, setHideMediumPeppers] = useState(false);
+  const [hideSpicyPeppers, setHideSpicyPeppers] = useState(false);
+  const [hideDeleteButton, setHideDeleteButton] = useState(true);
+
+
+
   const handleSpicinessChange = (passedSpiciness) => {
     setPostDraft((currDraft) => {
       const newPostDraft = { ...currDraft, spiciness: passedSpiciness };
-      console.log("MainModal.js: handleSpicinessChange: setting postDraft to", newPostDraft);
+      // console.log("MainModal.js: handleSpicinessChange: setting postDraft to", newPostDraft);
       return newPostDraft;
    });
+    setHideDeleteButton(false);
+  };
+
+  const resetSelectedSpiciness = (emptyString) => {
+    setPostDraft((currDraft) => {
+      const newPostDraft = { ...currDraft, spiciness: emptyString };
+      // console.log("MainModal.js: handleSpicinessChange: setting postDraft to", newPostDraft);
+      return newPostDraft;
+   });
+    setHideDeleteButton(true);
   };
 
 
-
-  // Declare what the component renders
   return (
     <>
       <div
         name="pepper-box"
-        style={{
-          display:"flex",
-          flexDirection:"column",
-          alignItems:"flex-end",
-        }}
+        className="flex flex-col"
       >
         <div
           name="mild-pepper-group"
-          className={postDraft.spiciness == "mild" ? "opacity-1" : "opacity-50"}
-          onClick={() => {
-            handleSpicinessChange("mild")
-          }}
+          className={postDraft.spiciness == "mild" ? "opacity-100 flex items-center" : "opacity-50 cursor-pointer"}
+          hidden={hideMildPepper}
         >
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'green',
-              cursor:"pointer"
+          <div
+            name = "pepperOnClickHousing"
+            className="flex justify-end"
+            onClick={() => {
+              handleSpicinessChange("mild");
+              setHideMediumPeppers(true);
+              setHideSpicyPeppers(true);
             }}
-          />
+          >
+            <GiChiliPepper
+              size="24"
+              className="text-green-600"
+            />
+          </div>
+          <div 
+            name="DltBtnOnClickHousing"
+            onClick = {() => {
+              resetSelectedSpiciness("");
+              setHideMediumPeppers(false);
+              setHideSpicyPeppers(false);
+            }}
+          >
+            <TiDeleteOutline
+              className="cursor-pointer text-gray-400"
+              size="16"
+              hidden = {hideDeleteButton}
+            />
+          </div>
         </div>
         <div
           name="medium-peppers-group"
-          className={postDraft.spiciness == "medium" ? "opacity-1" : "opacity-50"}
-          style={{display:"flex"}}
-          onClick={() => {
-            handleSpicinessChange("medium")
-          }}
+          className={postDraft.spiciness == "medium" ? "opacity-100 flex items-center" : "opacity-50 cursor-pointer"}
+          hidden={hideMediumPeppers}
         >
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'orange',
-              cursor:"pointer"
+          <div
+            name="pepperOnClickHousing"
+            className="flex justify-end"
+            onClick={() => {
+              handleSpicinessChange("medium");
+              setHideMildPepper(true);
+              setHideSpicyPeppers(true);
             }}
-          />
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'orange',
-              cursor:"pointer"
+          >
+            <GiChiliPepper
+              size="24"
+              className="text-yellow-500"
+            />
+            <GiChiliPepper
+              size="24"
+              className="text-yellow-500"
+            />
+          </div>
+          <div
+            name = "DltBtnOnClickHousing"
+            onClick = {() => {
+              resetSelectedSpiciness("");
+              setHideMildPepper(false);
+              setHideSpicyPeppers(false);
             }}
-          />
+          >
+            <TiDeleteOutline
+              className="cursor-pointer text-gray-400"
+              size="16"
+              hidden = {hideDeleteButton}
+            />
+          </div>
         </div>
         <div
           name="spicy-peppers-group"
-          className={postDraft.spiciness == "spicy" ? "opacity-1" : "opacity-50"}
-          style={{display:"flex"}}
-          onClick={() => {
-            handleSpicinessChange("spicy")
-          }}
+          className={postDraft.spiciness == "spicy" ? "opacity-100 flex items-center" : "opacity-50 cursor-pointer"}
+          hidden={hideSpicyPeppers}
         >
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'red',
-              cursor:"pointer"
+          <div
+            name="pepperOnClickHousing"
+            className="flex justify-end"
+            onClick={() => {
+              handleSpicinessChange("spicy");
+              setHideMildPepper(true);
+              setHideMediumPeppers(true);
+              setHideDeleteButton(false)
             }}
-          />
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'red',
-              cursor:"pointer"
+          >
+            <GiChiliPepper
+              size="24"
+              className="text-red-500"
+            />
+            <GiChiliPepper
+              size="24"
+              className="text-red-500"
+            />
+            <GiChiliPepper
+              size="24"
+              className="text-red-500"
+            />
+          </div>
+          <div
+            name="DltBtnOnClickHousing"
+            onClick={() => {
+              resetSelectedSpiciness("");
+              setHideMildPepper(false);
+              setHideMediumPeppers(false);
             }}
-          />
-          <GiChiliPepper
-            size="24"
-            style={{
-              color:'red',
-              cursor:"pointer"
-            }}
-          />
+          >
+            <TiDeleteOutline
+              className="cursor-pointer text-gray-400"
+              size="16"
+              hidden = {hideDeleteButton}
+            />
+          </div>
         </div>
       </div>
     </>
