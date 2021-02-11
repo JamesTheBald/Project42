a82a5f9d1a3ca2b289119614792a1d8e8c7019e8
Opeyemi Obayemi <opeyemi.obayemi@gmail.com>
2021-02-11 16:43:56 -0700

    completed storing of stub locations in DB

--- zfront/src/components/RenderStubs.js
+++ zfront/src/components/RenderStubs.js
@@ -3,6 +3,8 @@ import Tooltip from "./Tooltip";
 import PopupContent from "./PopupContent";
 import Draggable from "react-draggable"
 import VoteCounter from "./VoteCounter";
+import updatePositionOnDB from "../functions/updatePositionOnDB";
+
 
 const RenderStubs = (props) => {
 
@@ -21,25 +23,36 @@ const RenderStubs = (props) => {
   // let userVoted = props.userVoted;
   // const setUserVoted = props.userVoted;
 
+  let defaultPosn = {};
 
-  const handlerOnStop = (post, index) => (event, data) => {    // Currying! Spicy! 
+  // useEffect( () => {
+  //   defaultPosn = {x:100,y:100};
+  // }, [])
+
+
+  const handleOnStop = (post, index) => (event, data) => {    // Currying! Spicy! 
     // https://www.carlrippon.com/using-currying-to-pass-additional-data-to-react-event-handlers/
     // console.log("post=",post);  console.log("index=",index);  console.log("event=",event);  console.log("data=",data);
 
     event.stopPropagation();             //J: I think this is important... don't want bubbling?
-    console.log("x=",data.x, " y=",data.y);
-    console.log("oldPosition.current.x = ", oldPosition.current.x, "oldPosition.current.y = ", oldPosition.current.y )
+    console.log("RenderStubs.js handleOnStop  x=",data.x, " y=",data.y);
+    console.log("RenderStubs.js handleOnStop  oldPosition.current.x = ", oldPosition.current.x, "oldPosition.current.y = ", oldPosition.current.y )
     
     if (data.x === oldPosition.current.x && data.y === oldPosition.current.y) {
-      console.log("You just clicked! Opening MainModal");
+      console.log("RenderStubs.js handleOnStop You just clicked! Opening MainModal");
 
       setCreatingPostFlag(false);
       setCurrPostIndex(index);
       console.log("RenderStubs.js CurrPostIndex=",index);
-      setPostDraft(post)
+      setPostDraft(post);
       setShowMainModal(true);
     } else {
-      // save the new coordinates (data.x, data.y) to DB, postingsDataArray[index].positionX and .positionY
+  
+      post.positionX = data.x;
+      post.positionY = data.y;
+      console.log("RenderStubs.js handleOnStop  post.positionX=", post.positionX, "  post.positionY=",post.positionY)
+      updatePositionOnDB(post, index);
+      defaultPosn = {x:post.positionX, y:post.positionY};
     }
 
     oldPosition.current = {x:data.x, y:data.y};
@@ -47,13 +60,17 @@ const RenderStubs = (props) => {
 
 
   console.log("RenderStubs.js postingsDataArray=", postingsDataArray);
+  console.log("RenderStubs.js postingsDataArray[0]._id=", postingsDataArray[0]._id);
 
-  if (postingsDataArray?.length>0) {
+
+  if (postingsDataArray && postingsDataArray[0]._id) {
     
     return (
       <>
-        {postingsDataArray.map((post, index) => {
-          // console.log("RenderStubs .map: index=", index, " and post=", post);
+        {postingsDataArray.map((post, index) => {           // console.log("RenderStubs .map: index=", index, " and post=", post);
+
+          defaultPosn = {x:post.positionX, y:post.positionY};
+          console.log("RenderStub.js .map loop  index=", index, "defaultPosn=", defaultPosn)
 
           return (
             <div key={index} className="w-64 my-2">
@@ -61,9 +78,9 @@ const RenderStubs = (props) => {
             {/* make <Draggable conditional on keypress? */}
               <Draggable
                 // bounds={bounds}
-                onStop={handlerOnStop(post, index)}
-                allowAnyClick={true}
-                // Specify location of the stub using post.positionX and post.positionY
+                onStop = {handleOnStop(post, index)}
+                allowAnyClick = {true}
+                defaultPosition = {defaultPosn}
               >
   
                   <div className="border p-2 border-gray-800 rounded-lg">
