50088f62cf698e3801a9b3149ab0002513c9adb0
James Murphy <jamesthebald@gmail.com>
2021-02-11 07:50:03 -0700

    Tooltip, draggable, no-right-click menu

--- zfront/src/components/PostingsList.js
+++ zfront/src/components/PostingsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
@@ -21,6 +21,8 @@ const emptyPost = {
   contentType: "",
   spiciness: "",
   upvotes: 0,
+  // positionX: 0,
+  // positionY: 0
 };
 
 const PostingsList = () => {
@@ -36,6 +38,12 @@ const PostingsList = () => {
   const [searchName, setSearchName] = useState("");
   const [searchTerm, setSearchTerm]=useState('Search by Title ');
   const [userVoted, setUserVoted] = useState(false);
+
+
+  useEffect(() => {
+    window.addEventListener('contextmenu', (evnt) => evnt.preventDefault(), false);
+    return () => window.removeEventListener('contextmenu', (evnt) => evnt.preventDefault(), false);
+  }, [])
 
 
   console.log("PostingsList.js begins: creatingPostFlag=", creatingPostFlag);
@@ -58,7 +66,7 @@ const PostingsList = () => {
 
 
   return (
-    <div>
+    <>
       {/* Navbar */}
       <nav className="w-full h-20 flex items-center text-blue-200 bg-blue-900">
         <div className="flex flex-row items-baseline">
@@ -89,12 +97,6 @@ const PostingsList = () => {
 
 
           <DropdownButton
-
-// Pass in setPostingsDataArray
-// import onClickFindByTitle, onClickFindByTags, onClickFindByName
-// Put in component's JS code: handleSelect 
-// Declare in component's JS code: searchTerm, searchTitle, searchTags, searchName,
- 
             id='dropdown-variants-outline-primary'    // Make this dropdown smaller. See Sizing: 
                                                       // https://react-bootstrap.netlify.app/components/dropdowns/#sizing 
             title={searchTerm}
@@ -188,37 +190,34 @@ const PostingsList = () => {
         setCreatingPostFlag = {setCreatingPostFlag}
       />
 
-    {showMainModal ? 
-      <MainModal 
-        showMainModal = {showMainModal}
-        setShowMainModal = {setShowMainModal}
-        postingsDataArray = {postingsDataArray}
-        setPostingsDataArray = {setPostingsDataArray}
-        currPostIndex = {currPostIndex}   //C: currPostIndex points to the element in the postings array that we're interested in
-        setCurrPostIndex = {setCurrPostIndex}
-        postDraft = {postDraft}
-        setPostDraft = {setPostDraft}
-        creatingPostFlag = {creatingPostFlag}
-        userVoted = {userVoted}
-        setUserVoted = {setUserVoted}
-      />
-      : (
-        <></>
-      )}
+      {showMainModal ? 
+        <MainModal 
+          showMainModal = {showMainModal}
+          setShowMainModal = {setShowMainModal}
+          postingsDataArray = {postingsDataArray}
+          setPostingsDataArray = {setPostingsDataArray}
+          currPostIndex = {currPostIndex}   //C: currPostIndex points to the element in the postings array that we're interested in
+          setCurrPostIndex = {setCurrPostIndex}
+          postDraft = {postDraft}
+          setPostDraft = {setPostDraft}
+          creatingPostFlag = {creatingPostFlag}
+          userVoted = {userVoted}
+          setUserVoted = {setUserVoted}
+        />
+        : ( <></> )
+      }
+        {/* <HomeButtonProvider />  */}  {/* For testing DnD coordinate reporting by react-draggable */}
 
-
-      {/* <HomeButtonProvider />  */}  {/* For testing DnD coordinate reporting by react-draggable */}
-
-      <Button 
-        variant="outline-danger"
-        onClick={() => {
-          removeAllPostings();
-          setPostingsDataArray([emptyPost]);
-        }}
-      >
-        [Dev Only] Remove All
-      </Button>
-    </div>
+        <Button 
+          variant="outline-danger"
+          onClick={() => {
+            removeAllPostings();
+            setPostingsDataArray([emptyPost]);
+          }}
+        >
+          [Dev Only] Remove All
+        </Button>
+    </>
   );
 };
 
--- zfront/src/components/RenderStubs.js
+++ zfront/src/components/RenderStubs.js
@@ -1,6 +1,6 @@
-import React, { useRef } from "react";
-// import Tooltip from "./Tooltip";
-// import PopupContent from "./PopupContent";
+import React, { useRef } from "react";    // , useEffect
+import Tooltip from "./Tooltip";
+import PopupContent from "./PopupContent";
 import Draggable from "react-draggable"
 
 
@@ -16,11 +16,11 @@ const RenderStubs = (props) => {
   // const bounds = { bottom: 0, right: 0 };
 
 
-  const handlerOnStop = (pst, indx) => (event, data) => {    // Currying! So spicy!
+  const handlerOnStop = (pst, indx) => (event, data) => {    // Currying! Spicy! 
+    // https://www.carlrippon.com/using-currying-to-pass-additional-data-to-react-event-handlers/
+    // console.log("pst=",pst);  console.log("index=",indx);  console.log("event=",event);  console.log("data=",data);
 
-    console.log("pst=",pst);  console.log("index=",indx);  console.log("event=",event);  console.log("data=",data);
-
-    event.stopPropagation();             //J: I think this is important. Don't want bubbling.?
+    event.stopPropagation();             //J: I think this is important... don't want bubbling?
     console.log("x=",data.x, " y=",data.y);
     console.log("oldPosition.current.x = ", oldPosition.current.x, "oldPosition.current.y = ", oldPosition.current.y )
     
@@ -32,9 +32,13 @@ const RenderStubs = (props) => {
       console.log("RenderStubs.js CurrPostIndex=",indx);
       setPostDraft(pst)
       setShowMainModal(true);
+    } else {
+      // save the new coordinates (data.x, data.y) to DB, postingsDataArray[indx].positionX and .positionY
     }
+
     oldPosition.current = {x:data.x, y:data.y};
   }
+
 
   console.log("RenderStubs.js postingsDataArray=", postingsDataArray);
 
@@ -48,35 +52,30 @@ const RenderStubs = (props) => {
           return (
             <div key={indx} className="w-64 my-2">
 
+            {/* make <Draggable conditional on keypress? */}
               <Draggable
                 // bounds={bounds}
                 onStop={handlerOnStop(pst, indx)}
+                allowAnyClick={true}
+                // Specify location of the stub using pst.positionX and pst.positionY
               >
-                {/* <Tooltip content={PopupContent(pst)} delay="200" direction="top" css="tooltipPopup rounded-lg">  */}
-                   {/* css="tooltipPopup" is required. Edit background color on tooltip.css */}
+                        {/* css="tooltipPopup" is required. Edit background color on tooltip.css */}
   
-                  <div
-                    className="border p-2 border-gray-800 rounded-lg"
-                    // onDoubleClick={() => {
-                      // setCreatingPostFlag(false);
-                      // setCurrPostIndex(indx);
-                      // console.log("RenderStubs.js CurrPostIndex=",indx);
-                      // setPostDraft(pst)
-                      // setShowMainModal(true);
-                    // }}
-                  >
-                    <div>
+                  <div className="border p-2 border-gray-800 rounded-lg">
+                    <Tooltip content={PopupContent(pst)} delay="200" direction="top" css="tooltipPopup rounded-lg"> 
+
+
                       { pst.title ? 
                         <div>{pst.title}</div>
                         :
                         <div> Click to edit </div>
                       }
                       <div className="mt-2">{pst.contributors}</div>
-                    </div>
 
+                    </Tooltip>
+                    
                   </div>
 
-                {/* </Tooltip> */}
               </Draggable>
             </div>
           );
