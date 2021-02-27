9d0e697722bad9487dee1440e1b36c4a8dfffcc0
James Murphy <jamesthebald@gmail.com>
2021-02-27 14:21:38 -0700

    Topics nicely formatted

--- zfront/src/components/TopicFormat.js
+++ zfront/src/components/TopicFormat.js
@@ -3,32 +3,25 @@ import React from 'react'
 const TopicFormat = (props) => {
 
   const topic = props.topic;
-  console.log("TopicFormat.js topic=", topic);
-
   const topicTitle = topic.topic || "Click to Edit"
-  console.log("TopicFormat.js topicLevel=", topic.topicLevel);
-
   let topicClassName = "";
 
   if (topic?.topicLevel) {
     
-    topicClassName = (topic.topicLevel === "Main Topic") ?
-        "text-7xl px-6  rounded-xl"
-      : (topic.topicLevel === "Sub-Topic") ?
-        "text-3xl px-4 py-1 rounded-lg"
-      :
-        "text-md px-2 py-1 rounded-md"
-
-    console.log("TopicFormat.js topicClassName=", topicClassName);
+    topicClassName = (topic.topicLevel === "Main Topic") ? "text-7xl px-6  rounded-xl"
+      : (topic.topicLevel === "Sub-Topic") ? "text-3xl px-4 py-1 rounded-lg"
+      : "text-md px-2 py-1 rounded-md"
   }
 
-
   return (
-      // <div className="">
-        <div className={`text-yellow-400 bg-gray-800 opacity-80 ${topicClassName}`}>
-          {topicTitle}
-        </div> 
-      // </div>
+    <div>
+      <div className={`text-yellow-300 relative z-10 ${topicClassName}`}>{topicTitle}</div>
+
+      <div className={`bg-gray-800 relative transform -translate-y-full opacity-60 z-0 ${topicClassName}`}>
+        {topicTitle}
+      </div>
+
+    </div> 
   )
 }
 
